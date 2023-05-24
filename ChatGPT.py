32785046558856199e908826bbed2d11e8f222ad
Avdhut Sagare <61410663+Avdhut16343@users.noreply.github.com>
2023-05-24 15:54:00 +0530

    Update ChatGPT.py
    added OPENAI_KEY os.getenv()

--- ChatGPT.py
+++ ChatGPT.py
@@ -1,9 +1,9 @@
 import openai 
-
+import os
 
 # Ashish API
 # openai.api_key ="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"
-openai.api_key ="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"
+openai.api_key =os.getenv('OPENAI_KEY')
 # openai.api_key ="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"
 model_engine = "text-davinci-003"
 
